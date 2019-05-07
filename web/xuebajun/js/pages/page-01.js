3fe8d33f00e2ba88c0f7a73cd9319616cf736de3
18832627144@163.com <18832627144@163.com>
2019-05-07 15:07:26 +0800

    update：学霸君落地页首图修改

--- web/xuebajun/js/pages/page-01.js
+++ web/xuebajun/js/pages/page-01.js
@@ -57,7 +57,7 @@ $(function () {
                     }
                 })
             }else{
-                console.log("stop!");
+                // console.log("stop!");
                 window.clearInterval(timer);
             }
         },1000);
